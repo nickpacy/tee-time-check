73d11d925ef4edd5a50c2f8ca3cfa8025420f5f2
Nick Pacy <nickpacy@gmail.com>
2023-08-01 18:44:52 -0700

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -13,17 +13,19 @@ app.use(express.json());
 app.use(cors());
 
 // Stage environment for server
-const stage = process.env.STAGE || 'default';
+const stage = process.env.STAGE;
 
-app.use(`/${stage}/auth`, authRoutes);
-app.use(`/${stage}/users`, usersRoutes);
-app.use(`/${stage}/courses`, coursesRoutes);
-app.use(`/${stage}/timechecks`, timechecksRoutes);
-app.use(`/${stage}/notifications`, notificationsRoutes);
+app.use(`${stage}/auth`, authRoutes);
+app.use(`${stage}/users`, usersRoutes);
+app.use(`${stage}/courses`, coursesRoutes);
+app.use(`${stage}/timechecks`, timechecksRoutes);
+app.use(`${stage}/notifications`, notificationsRoutes);
 
-// const PORT = process.env.PORT || 5050;
-// app.listen(PORT, () => {
-//   console.log(`Server listening on port ${PORT}`);
-// });
-
-module.exports.handler = serverless(app)
+if (process.env.STATIC == 'true') {
+    module.exports.handler = serverless(app) 
+} else {
+    const PORT = process.env.PORT || 5050;
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+}
