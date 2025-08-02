37b5bbf1d232f1df5231ae30e17adebdb224ad6e
Nick Pacy <nickpacy@gmail.com>
2025-08-01 21:37:37 -0700

    Update environment.dev.ts

--- frontend/src/environments/environment.dev.ts
+++ frontend/src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://api.dev.algotee.com/',
+  apiUrl: 'https://api.dev.algotee.com',
   logoUrl: 'https://d6l5cjqqn6ggt.cloudfront.net/logos/'
 };
